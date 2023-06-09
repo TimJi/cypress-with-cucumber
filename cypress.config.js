e30e830b7f767e3445778fc7ed2395207990a5c8
Tim Ji <timji@kktv.me>
2023-06-10 00:43:48 +0800

    bind cucumber

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,23 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
+const createEsbuildPlugin =
+  require('@badeball/cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor')
+const addCucumberPreprocessorPlugin =
+  require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin
 
 module.exports = defineConfig({
   projectId: 'your project id from https://cloud.cypress.io/',
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+    specPattern: ['**/*.{feature,features}', 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'],
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+      on(
+        'file:preprocessor',
+        createBundler({
+          plugins: [createEsbuildPlugin(config)]
+        })
+      );
+      return config;
+    }
+  }
 });
